1744a7ae9482749442c79e5560eb38702f86463d
aliefdany <official.alief.tech@gmail.com>
2023-02-25 15:14:40 +0700

    installed and configured tokyonight

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,20 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{
+		'folke/tokyonight.nvim',
+    priority = 500,
+    config = function()
+      vim.cmd.colorscheme 'tokyonight-night'
+			require('lualine').setup {
+     options = {
+        icons_enabled = false,
+        theme = 'tokyonight',
+        component_separators = '|',
+        section_separators = '',
+      },
+}
+       end,
+	}
+}
