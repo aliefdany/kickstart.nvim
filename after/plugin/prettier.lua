127e0860433be9bcb96ddf7617365671ab2cee0a
aliefdany <works@alief.tech>
2023-03-02 17:11:36 +0700

    updated prettier.lua

--- after/plugin/prettier.lua
+++ after/plugin/prettier.lua
@@ -17,7 +17,7 @@ require("null-ls").setup({
                 buffer = bufnr,
                 callback = function()
                     -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    vim.lsp.buf.format()
+                    vim.lsp.buf.format({ bufnr = bufnr })
                 end,
             })
         end
